ebc9b8bcbfab6a911ee38c23c30bf26f9476966b
Mike Emo <MEmo@mpifix.com>
2016-05-26 08:48:13 -0500

    Added Business Logic, 5 tests, 2 fail.

---
+++ BusinessLogic/BusinessManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class BusinessManager
+    {
+        public void DoBusinessStuff(Exception exceptionToThrow)
+        {
+            throw exceptionToThrow;
+        }
+
+        public bool CheckIfBusinessIsViable(bool resultToReturn)
+        {
+            return resultToReturn;
+        }
+    }
+}
---
+++ OctopusTesting/BusinessTests/BusinessManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+
+namespace OctopusTesting.BusinessTests
+{
+    [TestClass]
+    public class BusinessManagerTests
+    {
+        BusinessManager manager = new BusinessManager();
+
+        [TestMethod]
+        public void ThrowNullExceptionTest()
+        {
+            manager.DoBusinessStuff(new NullReferenceException("Throw null exception test"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AccessViolationException))]
+        public void ThrowNullExceptionTestExpected()
+        {
+            manager.DoBusinessStuff(new AccessViolationException("Throw null exception test"));
+        }
+
+        [TestMethod]
+        public void BusinessIsViableTrue()
+        {
+            Assert.IsTrue(manager.CheckIfBusinessIsViable(true));
+        }
+
+        [TestMethod]
+        public void BusinessIsNOTViableTrue()
+        {
+            Assert.IsTrue(manager.CheckIfBusinessIsViable(false));
+        }
+
+        [TestMethod]
+        public void BusinessIsNOT_NOTViableTrue()
+        {
+            Assert.IsTrue(manager.CheckIfBusinessIsViable(true), "The business is not-not viable");
+        }
+    }
+}
